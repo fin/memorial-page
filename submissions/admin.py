97f68e127601bddbd93b592a5c9b1c2e7def4b6a
fin <fin+github@xbhd.org>
2015-09-19 20:31:12 +0200

    admin first acceptance workflow, needs improvement

--- submissions/admin.py
+++ submissions/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
 from models import Submission, Image
+from django.core.exceptions import PermissionDenied
+from datetime import datetime
 
 class ImageInlineAdmin(admin.TabularInline):
     model = Image
 
 class SubmissionAdmin(admin.ModelAdmin):
     inlines = [ImageInlineAdmin]
+
+    actions = ['approve']
+    list_filter = ('accepted_at',)
+
+    def approve(self, request, queryset):
+        opts = self.model._meta
+        if not self.has_change_permission(request):
+            raise PermissionDenied
+        queryset.update(accepted_at=datetime.now(),
+                        accepted_by=request.user)
+        self.message_user(request, "Successfully marked submissions as accepted.")
+
+        return None
 
 admin.site.register(Submission, SubmissionAdmin)
--- submissions/models.py
+++ submissions/models.py
@@ -6,9 +6,9 @@ class Submission(models.Model):
     submitted_at = models.DateTimeField(null=True, blank=True)
     accepted_at = models.DateTimeField(null=True, blank=True)
     accepted_by = models.ForeignKey(User, null=True, blank=True, related_name='accepted_submissions')
-    message = models.TextField(blank=True, null=True, help_text='A private message to partners and family')
+    message = models.TextField(blank=True, null=True, help_text='If you\'d like ,leave a private message to partner and family (not public)')
     name = models.CharField(max_length=200,blank=True, null=True, help_text="Name, Location would be ideal")
-    email = models.CharField(max_length=200,blank=True, null=True, help_text="Give us a way to contact you")
+    email = models.CharField(max_length=200,blank=True, null=True, help_text="Give us a way to contact you (not public)")
 
     text = models.TextField(blank=True, help_text='A story about how he touched you, or anything else you want to share.')
 
