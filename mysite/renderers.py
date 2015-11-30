f5192fc95a29a94cec55f6e9f427d69adb7655a5
fin <fin+github@xbhd.org>
2015-11-30 23:25:05 +0100

    static site-baking, disable submission

--- mysite/renderers.py
+++ mysite/renderers.py
@@ -1,4 +1,5 @@
 from django_medusa.renderers import StaticSiteRenderer
+from submissions.models import Submission
 
 class HomeRenderer(StaticSiteRenderer):
     def get_paths(self):
@@ -7,4 +8,12 @@ class HomeRenderer(StaticSiteRenderer):
             "/donate/",
         ])
 
-renderers = [HomeRenderer, ]
+class ImagesRenderer(StaticSiteRenderer):
+    def get_paths(self):
+        submissions = Submission.objects.filter(accepted_at__isnull=False).order_by('-accepted_at')
+        paths = reduce(lambda x,y: x+y, [[image.file.url for image in submission.current_files] for submission in submissions], [])
+        print paths
+        return frozenset(paths)
+
+
+renderers = [HomeRenderer, ImagesRenderer]
